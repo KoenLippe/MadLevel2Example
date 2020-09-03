b494d7374dce5e38e6c59979eb510c7dd6d33a94
Koen Lippe <koen.lippe@quicknet.nl>
2020-09-03 17:39:40 +0200

    1.1 - Setup project and layout

--- app/src/main/java/com/example/madlevel2example/ReminderActivity.kt
+++ app/src/main/java/com/example/madlevel2example/ReminderActivity.kt
@@ -3,9 +3,10 @@ package com.example.madlevel2example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_reminder)
     }
 }
