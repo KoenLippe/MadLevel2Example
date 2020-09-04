b8d562bbf11340173d5d13f295c6ca848204605a
Koen Lippe <koen.lippe@quicknet.nl>
2020-09-04 15:11:40 +0200

    5 - Swipe left to delete functionality

--- app/src/main/java/com/example/madlevel2example/ReminderActivity.kt
+++ app/src/main/java/com/example/madlevel2example/ReminderActivity.kt
@@ -3,6 +3,7 @@ package com.example.madlevel2example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.adapter.ReminderAdapter
@@ -33,7 +34,8 @@ class ReminderActivity : AppCompatActivity() {
 
         binding.rvReminders.layoutManager = LinearLayoutManager(this@ReminderActivity, RecyclerView.VERTICAL, false)
         binding.rvReminders.adapter = reminderAdapter
-        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@ReminderActivity, DividerItemDecoration.VERTICAL));
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@ReminderActivity, DividerItemDecoration.VERTICAL))
+        createItemTouchHelper().attachToRecyclerView(binding.rvReminders)
 
     }
 
@@ -47,5 +49,24 @@ class ReminderActivity : AppCompatActivity() {
         }
     }
 
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
 
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+               return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+
+        return ItemTouchHelper(callback)
+    }
 }
